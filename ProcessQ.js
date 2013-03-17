cff005d13612e8a6d28d7e254e7fcc603c5ab49b
lbj96347 <cashlee96347@gmail.com>
2013-03-18 02:44:21 +0800

    [add]Add script loader

--- ProcessQ.js
+++ ProcessQ.js
@@ -207,7 +207,8 @@
 		constructor : ImageLoader,
 		id : null ,
 		start : function(queue){
-			var img=this.img=new Image();
+			var img= this.img =new Image();
+      console.log( this.img );
 			this.finished=this.async;
 			var Me=this;
 			function onload(event) {
@@ -309,8 +310,64 @@
 
 	};
 
+  /* add javascript source loader */
+
+	var JsLoader =function(cfg){
+		for (var key in cfg){
+			this[key]=cfg[key]
+		}
+	};
+
+	ProcessQ.types["script"]=JsLoader;
+
+  JsLoader.prototype = {
+		constructor : JsLoader,
+		id : null ,
+		async : false ,
+		start : function(queue){
+      console.log( this );
+      var script = this.script = new Image();
+			this.finished=this.async;
+			var Me=this;
+
+      //throw SyntaxError();
+
+			function onload(event) {
+				Me.finished=true;
+				this.removeEventListener("load", onload);
+			}
+			function onerror(event) {
+				Me.finished=false;
+				Me.errorEvent=event;
+				this.removeEventListener("error", onerror);
+			}
+
+			script.addEventListener("load", onload);
+			script.addEventListener("error", onerror);
+
+			script.setAttribute('src', this.src );
+      document.getElementById('hello').setAttribute('src', this.src );
+
+      var oHead = document.getElementsByTagName('HEAD').item(0);
+      var oScript= document.createElement("script");
+      oScript.type = "text/javascript";
+      oScript.src= this.src ;
+      oHead.appendChild( oScript);
+
+		},
+
+		getResult : function(){
+			return this.script;
+		},
+
+		isFinished : function(queue){
+			return this.finished;
+		},
+
+		isError : function(queue){
+			return this.errorEvent;
+		}
+  }
+
 
 }(this));
-
-
-
